14851633908f5ae6f366f2dbe22a68298ef6de0b
krishna sarone <167612353+krisn2@users.noreply.github.com>
2025-05-08 15:14:58 +0530

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 function App() {
 
   return (
-    <div className="bg[#463746] h-screen absolute inset-0">
+    <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
